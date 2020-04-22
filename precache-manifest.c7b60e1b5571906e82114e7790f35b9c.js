b135383570b6004181c6abf57508a7b1eefb7aa3
Jose Bautista <Josefbautista94@gmail.com>
2020-04-21 22:26:29 -0400

    Updates

---
+++ precache-manifest.c7b60e1b5571906e82114e7790f35b9c.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "15d1cf4abb57f0e84d954b32254ed279",
+    "url": "/master/index.html"
+  },
+  {
+    "revision": "0599477a3c4879b3b62c",
+    "url": "/master/static/css/main.a2be7963.chunk.css"
+  },
+  {
+    "revision": "71a217147fc2b994b6fe",
+    "url": "/master/static/js/2.15e37ec1.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/master/static/js/2.15e37ec1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0599477a3c4879b3b62c",
+    "url": "/master/static/js/main.43fbe879.chunk.js"
+  },
+  {
+    "revision": "3a1b90695831f97afcf1",
+    "url": "/master/static/js/runtime-main.c107e595.js"
+  },
+  {
+    "revision": "2c691434bb484e599c9a25f9e5562c8e",
+    "url": "/master/static/media/58485698e0bb315b0f7675a8.2c691434.png"
+  },
+  {
+    "revision": "41583ce94cd09f706019a37a5e004a4e",
+    "url": "/master/static/media/6405318ac146473a95bfbdcec2b32943.41583ce9.gif"
+  },
+  {
+    "revision": "2a98142a27dcabf5bfb379e21718ee2d",
+    "url": "/master/static/media/Copy of Jose Bautista f Resume 2019.docx 2.2a98142a.pdf"
+  },
+  {
+    "revision": "015b78748fe71b68d387cab65319b64a",
+    "url": "/master/static/media/IMG_4987.015b7874.jpg"
+  },
+  {
+    "revision": "80c1da793a2d278537f87383d4b59f91",
+    "url": "/master/static/media/IMG_7614 2.80c1da79.jpg"
+  },
+  {
+    "revision": "20e9aefb1f2c32500233e15a08a16943",
+    "url": "/master/static/media/ef536660637745.5a54876d6bbe1.20e9aefb.gif"
+  },
+  {
+    "revision": "85726374cf02c5ee91f130636fbcd610",
+    "url": "/master/static/media/home-transparent-white.85726374.png"
+  },
+  {
+    "revision": "16286484777c9d1466b2d26adfdee02c",
+    "url": "/master/static/media/joseBautista.16286484.jpeg"
+  }
+]);
